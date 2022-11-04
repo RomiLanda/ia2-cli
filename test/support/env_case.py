5bc19c68f3229de9b4f012dc09eb5c9b1a6d0ecb
RomiLanda <romailanda@gmail.com>
2022-11-04 13:27:00 -0300

    upadte docker-compose: run test

--- test/support/env_case.py
+++ test/support/env_case.py
@@ -21,8 +21,7 @@ class ModelSetup:
         Simple retrieval function.
         Returns MODEL_FILE or raises OSError.
         """
-        # model_path = os.getenv("TEST_MODEL_FILE", default=None)
-        model_path = "src/ia2/ia2/models/custom_model_test"
+        model_path = os.getenv("TEST_MODEL_FILE", default=None)
         if model_path is None:
             raise OSError("TEST_MODEL_FILE environment is not set.")
         return model_path
